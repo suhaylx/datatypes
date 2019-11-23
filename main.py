ee2282615a3682e7a9c46dbd19b26ea740fcad50
suhaylx <akmsodikov@gmail.com>
2019-11-23 06:22:47 +0000

    Math operations and Data Types

---
+++ main.py
@@ -0,0 +1,9 @@
+#Fundamental Data Types
+print(1+3)
+print(type(1+3))  #type of 1+3 is integer
+print(type(1/4))  # Floating point number 
+print(2**32)
+print (4//3) # 4 = (1*3)  +1  soo answer is 1 
+print(6//4) # 6 = (1*4)  +2  soo answer is 1 
+
+print(7%4) # 7 = 4 * 1 + 3 soo answer is 3 operatiom % finds remainder
